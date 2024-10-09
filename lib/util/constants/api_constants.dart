031563f65651a6d5c52175e7c2ac17ebd621c617
Enmanuel <emabenj_22@hotmail.com>
2024-10-08 22:37:31 -0500

    Modificación de ip en constants

--- lib/util/constants/api_constants.dart
+++ lib/util/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class APIConstants {
   // static const baseUrl = "http://localhost:8000/colegio";
   static const port = "8000";
-  static const ip = "192.168.18.6";//127.0.0.1
+  static const ip = "x.x.x.x";
   static const domain = "$ip:$port/";
   static const url = "http://$ip:$port";
   static const baseUrl = "http://${domain}colegio";
